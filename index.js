cf2144214560ba0277a8d436b02cd407c76363eb
marjutubli@gmail.com <marjutubli@gmail.com>
2018-04-29 19:51:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object = Object.assign(object, { [key]:value });
-  console.log(object)
+  console.log(object);
   return object;
+}
+
+function deleteFromObjectByKey(object, key) {
+  
 }
