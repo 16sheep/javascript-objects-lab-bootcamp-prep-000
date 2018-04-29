02dfac2dc3335787cfcd35ce1bd811c4a0975233
marjutubli@gmail.com <marjutubli@gmail.com>
2018-04-29 19:38:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  newObject = Object.assign({}, obj, {[key]:value});
+  newObject = Object.assign({}, object, {[key]:value});
   console.log(newObject)
   return newObject;
 }
